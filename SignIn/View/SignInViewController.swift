fe569e95db8813cba4b1081476e413acf4968e8c
Kaique Lopes <41844709+Kaique-Lopes@users.noreply.github.com>
2025-02-27 19:59:25 -0300

    Initial commit

---
+++ SignIn/View/SignInViewController.swift
@@ -0,0 +1,12 @@
+//
+//  SignInViewController.swift
+//  KingBurger
+//
+//  Created by Kaique Lopes on 27/02/25.
+//
+
+import UIKit
+
+class SignInViewController: UIViewController {
+    
+}
---
+++ ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  KingBurger
+//
+//  Created by Kaique Lopes on 27/02/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = UIColor.red
+    }
+
+
+}
+
